1ba99eaf6c51aee06c2c39aa4aceed889b880469
Minkyung <mminkyung.k@gmail.com>
2022-02-22 20:19:29 +0900

    Use TouchableOpacity and TouchableHighlight

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight } from 'react-native';
 import {theme} from "./colors";
 
 export default function App() {
@@ -7,6 +7,14 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity activeOpacity={0}>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableHighlight underlayColor='#DDDDDD' activeOpacity={0} onPress={() => console.log('pressed')}>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
 }
