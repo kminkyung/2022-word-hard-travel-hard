d5a9e2c6793ddbfb68321b2477150424c97ef07c
Minkyung <mminkyung.k@gmail.com>
2022-02-23 20:37:24 +0900

    Split styling object into a file

---
+++ styles.ts
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+import { theme } from './colors';
+
+export default StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: theme.bg,
+		paddingHorizontal: 20,
+	},
+	header: {
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		marginTop: 100,
+	},
+	btnText: {
+		fontSize: 38,
+		fontWeight: '600',
+		color: 'white'
+	},
+	input: {
+		backgroundColor: 'white',
+		paddingVertical: 15,
+		paddingHorizontal: 20,
+		borderRadius: 30,
+		marginVertical: 20,
+		fontSize: 18
+	},
+	todo: {
+		backgroundColor: theme.grey,
+		marginBottom: 10,
+		paddingVertical: 20,
+		paddingHorizontal: 40,
+		borderRadius: 15
+	},
+	todoText: {
+		color: 'white',
+		fontSize: 16,
+		fontWeight: '500'
+	}
+});
